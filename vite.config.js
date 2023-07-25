426a3f5f90173d7e5711b1211cd6d3cc7b1e5d66
Alanav29 <josephalannavarro@gmail.com>
2023-07-25 17:27:32 -0600

    cambios en vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "https://Alanav29.github.io/frontMascotas",
+	base: "https://alanav29.github.io/frontMascotas",
 });
