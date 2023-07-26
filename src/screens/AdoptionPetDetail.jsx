710697478c979611be8e4d74518da2635f87cb49
Ana Vera <108773954+ana-vera@users.noreply.github.com>
2023-07-25 21:14:44 -0600

    adoption pet detail, edit, add

---
+++ src/screens/AdoptionPetDetail.jsx
@@ -0,0 +1,19 @@
+import PetDetail from "../components/generalComponents/PetDetail";
+import delAdoptionPet from "../utils/adoptionPets/delAdoptionPet";
+import getComments from "../utils/comments/getComments";
+import postComment from "../utils/comments/postComment";
+import getAdoptionPet from "../utils/adoptionPets/getAdoptionPet";
+
+const AdoptionPetDetail = () => {
+	return (
+		<PetDetail
+			delPet={delAdoptionPet}
+			getComments={getComments}
+			postComment={postComment}
+			getPet={getAdoptionPet}
+			editUrl={"/mascota-en-adopcion-edit/"}
+		/>
+	);
+};
+
+export default AdoptionPetDetail;
