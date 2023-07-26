2fdd7c2763fcc47c39ba6092bb999cfbad41051a
Ana Vera <108773954+ana-vera@users.noreply.github.com>
2023-07-26 17:15:46 -0600

    adoption pet edit screen

--- src/screens/AdoptionPetEdit.jsx
+++ src/screens/AdoptionPetEdit.jsx
@@ -1,6 +1,17 @@
+import PetEdit from "../components/generalComponents/PetEdit";
+import getAdoptionPet from "../utils/adoptionPets/getAdoptionPet";
+import delAdoptionPet from "../utils/lostPets/delAdoptionPet";
+import putAdoptionPet from "../utils/lostPets/putAdoptionPet";
+
 const AdoptionPetEdit = () => {
-  return (
-    <div>AdoptionPetEdit</div>
-  )
-}
-export default AdoptionPetEdit
+	return (
+		<PetEdit
+			getPet={getAdoptionPet}
+			delPet={delAdoptionPet}
+			putPet={putAdoptionPet}
+			petTypeUrl={"/mascotas-en-adopcion/"}
+		/>
+	);
+};
+
+export default AdoptionPetEdit;
